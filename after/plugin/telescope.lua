760c85098d45a704f8342acb190b04c98f2a2dd0
thomas <tomgaek@gmail.com>
2024-12-13 22:14:21 +0000

    Setup telescope for directory lookup

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,3 +11,21 @@ vim.keymap.set('v', '<leader>ps', function()
     local selected_text = vim.fn.getreg('v')
     builtin.grep_string({ search = selected_text });
 end)
+
+-- Finding all directiories inside pwd:
+-- install: <sudo apt install fd-find> for this to work
+vim.keymap.set('n', '<leader>pd', function()
+    builtin.find_files({
+        find_command = { 'fdfind', '--type', 'd' },
+        cwd = vim.fn.getcwd(),
+        attach_mappings = function(_, map)
+            local actions = require('telescope.actions')
+            local action_state = require('telescope.actions.state')
+            map('n', '<CR>', function(prmopt_bufnr)
+                local entry = action_state.get_selected_entry()
+                vim.cmd('tabedit ' .. entry.path)
+            end)
+            return true
+        end,
+    })
+end, {noremap = true, silent = true})
